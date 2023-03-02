e69f16f26744a40fd3f63bad027c6eb0e1a107b5
tunacim <tunaz783@gmail.com>
2023-03-02 13:58:03 +0300

    musicApp

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {View,SafeAreaView,Text,StyleSheet} from "react-native"
+function App(){
+  return(
+    <SafeAreaView style={styles.container}>
+      <Text> </Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+container:{
+  flex:1
+}
+  })
+
+export default App;
